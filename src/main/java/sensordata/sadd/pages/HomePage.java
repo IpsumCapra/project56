9c603f4c7128a4c7ea198dc0c22c87ff9ecb40d3
Chiaraaw <0993153@hr.nl>
2020-11-23 15:42:19 +0100

    Added search bar.

--- src/main/java/sensordata/sadd/pages/HomePage.java
+++ src/main/java/sensordata/sadd/pages/HomePage.java
@@ -1,4 +1,9 @@
 package main.java.sensordata.sadd.pages;
 
+import javax.swing.*;
+
 public class HomePage {
+    private JButton searchButton;
+    private JPanel panel1;
+    private JTextField searchTextField;
 }
